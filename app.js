adb6f6823814ac1bd2df535a45eacb5f075acace
AristonCatipay <aristoncatipay123@gmail.com>
2024-09-29 00:46:02 +0800

    Update the style of landing page.

--- app.js
+++ app.js
@@ -71,16 +71,16 @@ function retrieveScore() {
 retrieveScore();
 
 // Prevent Inspect Element
-document.addEventListener("contextmenu", function (e) {
-  e.preventDefault();
-});
+// document.addEventListener("contextmenu", function (e) {
+//   e.preventDefault();
+// });
 
-document.addEventListener("keydown", function (e) {
-  if (
-    e.keyCode == 123 ||
-    (e.ctrlKey && e.shiftKey && e.keyCode == 73) ||
-    (e.ctrlKey && e.keyCode == 85)
-  ) {
-    e.preventDefault();
-  }
-});
+// document.addEventListener("keydown", function (e) {
+//   if (
+//     e.keyCode == 123 ||
+//     (e.ctrlKey && e.shiftKey && e.keyCode == 73) ||
+//     (e.ctrlKey && e.keyCode == 85)
+//   ) {
+//     e.preventDefault();
+//   }
+// });
